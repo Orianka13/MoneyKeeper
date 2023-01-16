f25200a04045b53eeb0750e63e7166463c535787
Orianka13 <odobrogor@gmail.com>
2023-01-16 14:43:16 +0400

    initial commit

---
+++ MoneyKeeper/MoneyKeeper/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  MoneyKeeper
+//
+//  Created by Олеся Егорова on 16.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ MoneyKeeper/MoneyKeeper/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MoneyKeeper
+//
+//  Created by Олеся Егорова on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
